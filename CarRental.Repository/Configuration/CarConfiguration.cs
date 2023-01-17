f13c52f08ec77946dbd23c91c9ddb07f19171b21
İsmail Temel <64991440+ismailtemel@users.noreply.github.com>
2023-01-17 19:09:04 +0300

    Add Configuration Tool

---
+++ CarRental.Repository/Configuration/CarConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using CarRental.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Repository.Configuration
+{
+    public class CarConfiguration : IEntityTypeConfiguration<Car>
+    {
+        public void Configure(EntityTypeBuilder<Car> builder)
+        {
+            builder.Property(x => x.RentalPrice).IsRequired().HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ CarRental.Repository/Configuration/RentalCategoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using CarRental.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Repository.Configuration
+{
+    public class RentalCategoryConfiguration : IEntityTypeConfiguration<RentalCategory>
+    {
+        public void Configure(EntityTypeBuilder<RentalCategory> builder)
+        {
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+        }
+    }
+}
